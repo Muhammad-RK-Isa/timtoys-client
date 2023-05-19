b969ee2602a680698bef7fd2b93a62d2b6351a09
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-05-20 04:46:17 +0600

    updated the theme

--- src/main.jsx
+++ src/main.jsx
@@ -3,31 +3,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import CreateUser from './Components/CreateUser.jsx';
-import UpdateUser from './Components/UpdateUser.jsx';
-import Home from './Components/Home.jsx';
 
 
 const routes = createBrowserRouter( [
   {
     path: '/',
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-        loader: () => fetch('http://192.168.0.179:5000/users')
-      },
-      {
-        path: '/create_user',
-        element: <CreateUser />
-      },
-      {
-        path: '/update_user/:id',
-        element: <UpdateUser />,
-        loader: ({params}) => fetch(`http://192.168.0.179:5000/user/${params._id}`)
-      }
-    ]
+    element: <App />
   }
 ] );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [ "./src/**/*.{jsx, js}" ],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+
+          "primary": "#824f40",
+
+          "secondary": "#ffffff",
+
+          "accent": "#3e261e",
+
+          "neutral": "#eef5f7",
+
+          "base-100": "#FFFFFF",
+
+          "info": "#3ABFF8",
+
+          "success": "#36D399",
+
+          "warning": "#FBBD23",
+
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [require("daisyui")],
+  plugins: [ require( "daisyui" ) ],
 }
 
