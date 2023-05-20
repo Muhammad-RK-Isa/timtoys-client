17240bd6d3c2a0a465dee96861db84c9874a4334
Muhammad-RK-Isa <muhammad-rk-isa@outlook.com>
2023-05-21 02:43:52 +0600

    made responsive banner/carousel slider

--- src/Pages/Home/Slider.jsx
+++ src/Pages/Home/Slider.jsx
@@ -1,136 +1,145 @@
 import { AiOutlineArrowLeft, AiOutlineArrowRight } from "react-icons/ai";
 import { Carousel, IconButton } from "@material-tailwind/react";
 import { ArrowLeftIcon, ArrowRightIcon } from "@heroicons/react/24/outline";
+import { useEffect, useState } from "react";
 
 const Slider = () => {
-    return (
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:max-w-5xl mx-4 md:mx-auto my-4 lg:my-6">
 
-            <Carousel
-                className="rounded-xl"
-                prevArrow={ ( { handlePrev } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handlePrev }
-                        className="!absolute top-2/4 -translate-y-2/4 left-4"
+    const [ dataSet1, setDataSet1 ] = useState( null );
+    const [ dataSet2, setDataSet2 ] = useState( null );
+    const [ dataSet3, setDataSet3 ] = useState( null );
+
+    useEffect( () => {
+        const requestdata = async ( category, dataSet ) => {
+            try {
+                const response = await fetch( `http://192.168.0.179:5000/products/random?quantity=3&category=${ encodeURIComponent( category ) }` );
+                const data = await response.json();
+                dataSet( data );
+            } catch ( error ) {
+                console.log( error );
+            }
+        };
+
+        requestdata( 'Preschool', setDataSet1 );
+        requestdata( 'Novelty & Gag Toys', setDataSet2 );
+        requestdata( 'Arts & Crafts', setDataSet3 );
+
+    }, [] );
+
+    if ( dataSet1 && dataSet2 && dataSet3 ) {
+        return (
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:max-w-5xl mx-4 md:mx-auto my-4 lg:my-6">
+                <div className="relative">
+                    <Carousel
+                        className="rounded-xl h-64 md:h-72 border-2"
+                        prevArrow={ ( { handlePrev } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handlePrev }
+                                className="!absolute top-2/4 -translate-y-2/4 left-4 bg-accent bg-opacity-25"
+                            >
+                                <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
+                        nextArrow={ ( { handleNext } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handleNext }
+                                className="!absolute top-2/4 -translate-y-2/4 !right-4  bg-accent bg-opacity-25"
+                            >
+                                <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
                     >
-                        <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-                nextArrow={ ( { handleNext } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handleNext }
-                        className="!absolute top-2/4 -translate-y-2/4 !right-4"
+                        {
+                            dataSet1.map( ( product ) => {
+                                return <img key={ product._id } src={ product.thumbnailImage } alt="carousel-image" className="h-full w-full object-cover" />;
+                            } )
+                        }
+                    </Carousel>
+                    <button className="absolute top-2 left-2 md:left-3 md:top-3 text-white font-bold bg-accent bg-opacity-40 p-2 rounded ">
+                        { dataSet1[ 0 ].category }
+                    </button>
+                </div>
+                <div className="relative hidden md:flex">
+                    <Carousel
+                        className="rounded-xl h-64 md:h-72 border-2"
+                        prevArrow={ ( { handlePrev } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handlePrev }
+                                className="!absolute top-2/4 -translate-y-2/4 left-4 bg-accent bg-opacity-25"
+                            >
+                                <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
+                        nextArrow={ ( { handleNext } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handleNext }
+                                className="!absolute top-2/4 -translate-y-2/4 !right-4  bg-accent bg-opacity-25"
+                            >
+                                <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
                     >
-                        <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-            >
-                <img
-                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                    alt="image 1"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                    alt="image 2"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                    alt="image 3"
-                    className="h-full w-full object-cover"
-                />
-            </Carousel>
-            <Carousel
-                className="rounded-xl hidden md:flex"
-                prevArrow={ ( { handlePrev } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handlePrev }
-                        className="!absolute top-2/4 -translate-y-2/4 left-4"
+                        {
+                            dataSet2.map( ( product ) => {
+                                return <img key={ product._id } src={ product.thumbnailImage } alt="carousel-image" className="h-full w-full object-cover" />;
+                            } )
+                        }
+                    </Carousel>
+                    <button className="absolute top-2 left-2 md:left-3 md:top-3 text-white font-bold bg-accent bg-opacity-40 p-2 rounded ">
+                        { dataSet2[ 0 ].category }
+                    </button>
+                </div>
+                <div className="relative hidden lg:flex">
+                    <Carousel
+                        className="rounded-xl h-64 md:h-72 border-2"
+                        prevArrow={ ( { handlePrev } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handlePrev }
+                                className="!absolute top-2/4 -translate-y-2/4 left-4 bg-accent bg-opacity-25"
+                            >
+                                <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
+                        nextArrow={ ( { handleNext } ) => (
+                            <IconButton
+                                variant="text"
+                                color="white"
+                                size="lg"
+                                onClick={ handleNext }
+                                className="!absolute top-2/4 -translate-y-2/4 !right-4  bg-accent bg-opacity-25"
+                            >
+                                <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6 hover:text-accent flex" />
+                            </IconButton>
+                        ) }
                     >
-                        <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-                nextArrow={ ( { handleNext } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handleNext }
-                        className="!absolute top-2/4 -translate-y-2/4 !right-4"
-                    >
-                        <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-            >
-                <img
-                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                    alt="image 1"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                    alt="image 2"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                    alt="image 3"
-                    className="h-full w-full object-cover"
-                />
-            </Carousel>
-            <Carousel
-                className="rounded-xl hidden lg:flex"
-                prevArrow={ ( { handlePrev } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handlePrev }
-                        className="!absolute top-2/4 -translate-y-2/4 left-4"
-                    >
-                        <ArrowLeftIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-                nextArrow={ ( { handleNext } ) => (
-                    <IconButton
-                        variant="text"
-                        color="white"
-                        size="lg"
-                        onClick={ handleNext }
-                        className="!absolute top-2/4 -translate-y-2/4 !right-4"
-                    >
-                        <ArrowRightIcon strokeWidth={ 2 } className="w-6 h-6" />
-                    </IconButton>
-                ) }
-            >
-                <img
-                    src="https://images.unsplash.com/photo-1497436072909-60f360e1d4b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2560&q=80"
-                    alt="image 1"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1493246507139-91e8fad9978e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80"
-                    alt="image 2"
-                    className="h-full w-full object-cover"
-                />
-                <img
-                    src="https://images.unsplash.com/photo-1518623489648-a173ef7824f3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2762&q=80"
-                    alt="image 3"
-                    className="h-full w-full object-cover"
-                />
-            </Carousel>
-        </div>
-    );
+                        {
+                            dataSet3.map( ( product ) => {
+                                return <img key={ product._id } src={ product.thumbnailImage } alt="carousel-image" className="h-full w-full object-cover" />;
+                            } )
+                        }
+                    </Carousel>
+                    <button className="absolute top-2 left-2 md:left-3 md:top-3 text-white font-bold bg-accent bg-opacity-40 p-2 rounded ">
+                        { dataSet3[ 0 ].category }
+                    </button>
+                </div>
+            </div>
+        );
+    }
 };
 
 export default Slider;
